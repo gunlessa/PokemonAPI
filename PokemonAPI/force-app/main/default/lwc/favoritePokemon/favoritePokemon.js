0228176b92b202f62d1dde3298dc6d54d0481db5
gunlessa <gunlessa@hotmail.com>
2022-02-25 15:20:17 -0300

    Pokemon img component

---
+++ PokemonAPI/force-app/main/default/lwc/favoritePokemon/favoritePokemon.js
@@ -0,0 +1,5 @@
+import { LightningElement, track } from 'lwc';
+
+export default class FavoritePokemon extends LightningElement {
+    @track pokemonImg = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png';
+}
