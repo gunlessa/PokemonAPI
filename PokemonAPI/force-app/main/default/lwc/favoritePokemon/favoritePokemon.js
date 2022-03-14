6f25fe6adf9345b04e92fffa8d65e36c64820e96
gunlessa <gunlessa@hotmail.com>
2022-03-14 19:59:30 -0300

    Unfinished

--- PokemonAPI/force-app/main/default/lwc/favoritePokemon/favoritePokemon.js
+++ PokemonAPI/force-app/main/default/lwc/favoritePokemon/favoritePokemon.js
@@ -1,5 +1,7 @@
-import { LightningElement, track } from 'lwc';
-
+import { LightningElement, track, wire} from 'lwc';
+import from @salesforce/apex/Pokemon;
 export default class FavoritePokemon extends LightningElement {
     @track pokemonImg = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png';
+    @wire 
+    
 }
