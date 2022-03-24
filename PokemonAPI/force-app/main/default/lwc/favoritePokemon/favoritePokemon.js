534e6284b8a69707714cde85f9a4c2740d46b4d9
gunlessa <gunlessa@hotmail.com>
2022-03-24 17:10:20 -0300

    favoritePokemon js

--- PokemonAPI/force-app/main/default/lwc/favoritePokemon/favoritePokemon.js
+++ PokemonAPI/force-app/main/default/lwc/favoritePokemon/favoritePokemon.js
@@ -1,7 +1,25 @@
-import { LightningElement, track, wire} from 'lwc';
-import from @salesforce/apex/Pokemon;
+import { LightningElement, api, track} from 'lwc';
+import getPokemonDetails from '@salesforce/apex/ShowPokemonController.getPokemonDetails';
 export default class FavoritePokemon extends LightningElement {
-    @track pokemonImg = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png';
-    @wire 
+    @track pokemonImg;
+    @api recordId;
     
+    
+    renderedCallback(){
+        this.getPokemon();
+    }
+
+    getPokemon() {
+        getPokemonDetails({contactID : this.recordId})
+            .then(result => {
+                console.log(JSON.stringify(result));
+                pokemonImg = result.sprites.front_default;
+            })
+            .catch(error => {
+                console.log(JSON.stringify(error));
+            });
+    }
+    
+    console.log(pokemonImg);
+
 }
